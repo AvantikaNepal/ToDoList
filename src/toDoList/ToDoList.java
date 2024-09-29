0fab3999ad0286c2bad538cd7b00b12da184b36b
acer <acer@172.16.40.1>
2024-09-29 22:41:21 +0545

    Arraylist added and accessed in the main method

--- src/toDoList/ToDoList.java
+++ src/toDoList/ToDoList.java
@@ -1,8 +1,11 @@
 package toDoList;
 
+import java.util.ArrayList;
+
 public class ToDoList {
 	String description;
 	boolean isComplete;
+	static ArrayList<ToDoList> taskList = new ArrayList<>();   //static so that all the instances of the class can access it  and it can be accessed without creating an instance of a class.
 	public ToDoList(String description) {
 		this.description = description;
 		this.isComplete = false;
@@ -15,10 +18,19 @@ public class ToDoList {
 		return description+(isComplete ? "(complete)" : "(pending)");
 		
 	}
+	public void printTask() {
+		System.out.println(description);
+	
+	}
 	public static void main(String[] args) {
-		ToDoList task1 = new ToDoList("buy groceries");
-		System.out.println(task1);
-		task1.taskComplete();
-		System.out.println(task1);
+		taskList.add(new ToDoList("buy groceries"));
+		taskList.add(new ToDoList("Do tha laundry"));
+		taskList.add(new ToDoList("Exercise for 30 mins"));
+		taskList.get(0).printTask();  //prints the first description
+		for(ToDoList tasks : taskList) {
+			tasks.printTask();    //print all the description
+			System.out.println(tasks); //directly object reference  is passed so it will by default call toString() method
+		}
+
 	}
 }
